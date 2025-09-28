4573743e0c9076b258314419ac3d8c093def2b38
BMwendwaM <mwendwa600@gmail.com>
2025-09-28 13:01:14 +0300

    Fix future_date variable.

--- fns_and_dsa/explore_datetime.py
+++ fns_and_dsa/explore_datetime.py
@@ -11,7 +11,7 @@ display_current_datetime()
 from datetime import timedelta
 number = int(input("Enter the number of days to add to the current date: "))
 def calculate_future_date():
-    today = datetime.date.today()
+    today = datetime.datetime.now()
     future_date = today + timedelta(days= number)
     print(future_date.strftime("%Y-%m-%d"))
 
