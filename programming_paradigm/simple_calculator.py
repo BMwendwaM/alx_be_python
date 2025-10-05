60b6c3f3b2949d1d1c615bdb75e4b2420cdf3773
BMwendwaM <mwendwa600@gmail.com>
2025-10-05 14:45:06 +0300

    Add simple_calculator.py

---
+++ programming_paradigm/simple_calculator.py
@@ -0,0 +1,16 @@
+#A simple calculator class that supports basic arithmetic operations."""
+
+class SimpleCalculator: 
+    def add(self, a, b):
+        return a + b
+    
+    def subtract(self, a, b):
+        return a - b
+
+    def multiply(self, a, b):
+        return a * b
+
+    def divide(self, a, b):
+        if b == 0:
+            return None
+        return a / b
