9b2469a21f2dd99573623494ea8e8938ba4be7fc
BMwendwaM <mwendwa600@gmail.com>
2025-10-05 18:25:29 +0300

    Fix list to private.

--- programming_paradigm/library_management.py
+++ programming_paradigm/library_management.py
@@ -12,21 +12,21 @@ class Book:
 
 class Library:
     def __init__(self):      
-        self.__books = []
+        self._books = []
 
     def add_book(self, book):
-        self.__books.append(book)
+        self._books.append(book)
 
     def check_out_book(self, title):
-        for book in self.__books:
+        for book in self._books:
             if book.title == title:
                 book.check_out()
 
     def return_book(self, title):
-        for book in self.__books:
+        for book in self._books:
             if book.title == title:
                 book.return_book()
     
     def list_available_books(self):
-        for book in self.__books:
+        for book in self._books:
             return book
